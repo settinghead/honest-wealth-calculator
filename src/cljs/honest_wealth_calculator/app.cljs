ab11081f8847c144d650f6bffb5ab6ca810d722b
Xiyang Chen <settinghead@gmail.com>
2015-07-11 00:19:39 -0400

    ownership cost

--- src/cljs/honest_wealth_calculator/app.cljs
+++ src/cljs/honest_wealth_calculator/app.cljs
@@ -6,25 +6,27 @@
 (enable-console-print!)
 
 (def data (atom
-  {:total-mortgage 400000
-   :interest-rate 0.048
-   :number-of-years 25}))
+  {:purchase-price 500000
+   :total-mortgage 400000
+   :interest-rate 0.065
+   :number-of-years 25
+   :ownership-cost-rate 0.04}))
 
 (defn input-row [label input]
-  [:div.row
-   [:div.col-md-2 [:label label]]
-   [:div.col-md-5 input]])
+  [:div.form-group
+   [:div.col-sm-2.control-label [:label label]]
+   [:div.col-sm-10 input]])
 
 (defn input [label type id]
   (input-row label [:input.form-control {:field type :id id}]))
 
-(defn money-input [label id placeholder init-value on-change]
-    [:div {:class "form-group"}
-        [:label {:class "sr-only", :for id} label]
-        [:div {:class "input-group"}
-         [:div {:class "input-group-addon"} "$"]
-         [:input {:type "text", :class "form-control", :id id, :placeholder placeholder :value init-value :on-change on-change}]
-         [:div {:class "input-group-addon"} ".00"]]])
+(defn money-input [label id placeholder init-value on-change read-only]
+  [:div
+    [:label {:class "sr-only", :for id} label]
+    [:div {:class "input-group"}
+     [:div {:class "input-group-addon"} "$"]
+     [:input {:type "text", :class "form-control", :step "1" :id id, :placeholder placeholder :value init-value :on-change on-change :read-only read-only}]
+     [:div {:class "input-group-addon"} ".00"]]])
 
 (defn to-rent []
   [:div
@@ -33,18 +35,34 @@
 (defn calc-monthly-payment []
   (let [total (@data :total-mortgage)
         interest (@data :interest-rate)
-        months (* 12 (@data :number-of-years))]
-      (mortgage/calc total interest months)))
+        months (* 12 (@data :number-of-years))
+        result (mortgage/calc total interest months)]
+      result))
+
+(defn calc-ownership-cost []
+  (let [total (@data :purchase-price)
+        rate (@data :ownership-cost-rate)]
+    (* total rate)))
 
 (defn to-own []
-  [:form {:class "form"}
-    [:h4 "If I own..."]
-    (input-row "Total mortgage" [money-input "Total mortgage" :total-mortgage "" (:total-mortgage @data) #(swap! data assoc :total-mortgage (.-target.value %))])
-    (input-row "Interest rate"
-      [:input.form-control {:field :numeric :id :interest-rate :value (:interest-rate @data) :on-change #(swap! data assoc :interest-rate (.-target.value %))}])
-    (input-row "Number of years"
-      [:input.form-control {:field :numeric :id :number-of-years :value (:number-of-years @data)}])
-    (input-row "Monthly payments" [:input.form-control {:field :numeric :fmt "%.2f" :id :bmi :read-only true :value (calc-monthly-payment)}])])
+  (let [result (calc-monthly-payment)
+        monthly (int (:monthly-payment result))
+        interest-cost (int (:interest-cost result))
+        ownership-cost (calc-ownership-cost)]
+    [:form {:class "form-horizontal"}
+      [:h4 "If I own..."]
+      (input-row "Purchase price" [money-input "Purchase price" :purchase-price "" (:purchase-price @data) #(swap! data assoc :purchase-price (.-target.value %)) false])
+      (input-row "Total mortgage" [money-input "Total mortgage" :total-mortgage "" (:total-mortgage @data) #(swap! data assoc :total-mortgage (.-target.value %)) false])
+      (input-row "Interest rate"
+        [:input.form-control {:field :numeric :id :interest-rate :value (:interest-rate @data) :on-change #(swap! data assoc :interest-rate (.-target.value %))}])
+      (input-row "Number of years"
+        [:input.form-control {:field :numeric :id :number-of-years :value (:number-of-years @data) :on-change #(swap! data assoc :number-of-years (.-target.value %))}])
+      (input-row "Monthly payments" [money-input "Monthly payments" :monthly-payments "" monthly nil true])
+      (input-row "Total interest cost" [money-input "Interst cost" :monthly-payments "" interest-cost nil true])
+      (input-row "Ownership cost rate"
+        [:input.form-control {:field :numeric :id :ownership-cost-rate :value (:ownership-cost-rate @data) :on-change #(swap! data assoc :ownership-cost-rate (.-target.value %))}])
+      (input-row "Annual ownership cost" [money-input "Annual ownership cost" :ownership-cost "" (calc-ownership-cost) nil true])
+      ]))
 
 (defn rent-or-own-calculator []
   [:div.container
@@ -55,7 +73,7 @@
 
 (defn main-component []
   [:div
-    [:h1 "The Honest Wealth Calculator"]
+    [:h1 "Wealth Projector"]
     [rent-or-own-calculator]])
 
 (defn init []
--- src/cljs/honest_wealth_calculator/mortgage.cljs
+++ src/cljs/honest_wealth_calculator/mortgage.cljs
@@ -3,5 +3,7 @@
 (defn calc [amount interest months]
   (let [monthly-interest (/ interest 12)
         acc (* monthly-interest (.pow js/Math (+ 1 monthly-interest) months))
-        compound (- (.pow js/Math (+ 1 monthly-interest) months) 1)]
-    (* amount (/ acc compound))))
+        compound (- (.pow js/Math (+ 1 monthly-interest) months) 1)
+        monthly-payment (* amount (/ acc compound))]
+    {:monthly-payment monthly-payment
+     :interest-cost (- (* monthly-payment months) amount)}))
