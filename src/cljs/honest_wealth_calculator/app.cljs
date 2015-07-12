77c375e0bdd18944779206670afc4d5e7d4ee8f7
Xiyang Chen <settinghead@gmail.com>
2015-07-12 02:15:06 -0400

    calc investment return

--- src/cljs/honest_wealth_calculator/app.cljs
+++ src/cljs/honest_wealth_calculator/app.cljs
@@ -13,6 +13,7 @@
    :monthly-lease-income 2500
    :monthly-rent 2500
    :percentage-invest 100
+   :investment-return-rate 5.0
    :ownership-cost-rate 3}))
 
 (defn input-row [label input]
@@ -67,6 +68,9 @@
                         (when (not= param :bmi)
                           (swap! data assoc :bmi nil)))}])
 
+(defn calc-roi [base annual-rate years]
+  (* (.pow js/Math (+ 1 annual-rate) years) base))
+
 (defn to-rent []
   (let [result (calc-monthly-payment)
         purchase-price (:purchase-price @data)
@@ -74,31 +78,31 @@
         total-down-payment (- purchase-price total-mortgage)
         percentage-invest (:percentage-invest @data)
         money-to-invest (/ (* total-down-payment percentage-invest) 100)
-        monthly (int (:monthly-payment result))
+        investment-return-rate (:investment-return-rate @data)
         years (:number-of-years @data)
+        total-roi (calc-roi money-to-invest (/ investment-return-rate 100) years)
+        monthly (int (:monthly-payment result))
         months (* 12 years)
         interest-cost (int (:interest-cost result))
         annual-ownership-cost (calc-ownership-cost)
-        monthly-rent (:monthly-rent @data)]
-    [panel-with-heading "If rent (and invest)..."
+        monthly-rent (:monthly-rent @data)
+        annual-rent (* 12 monthly-rent)
+        total-rent (* years annual-rent)]
+    [panel-with-heading "If rent (plus investments)..."
         [:form {:class "form-horizontal"}
           (input-row "Down payment saved" [money-input "Down payment saved" :down-payment "" total-down-payment nil true])
-        ;   (input-row "Total mortgage" [money-input "Total mortgage" :total-mortgage "" (:total-mortgage @data) #(swap! data assoc :total-mortgage (.-target.value %)) false])
-        ;   (input-row "Annual interest rate" [percentage-input "Annual interest rate" :interest-rate "" (:interest-rate @data) #(swap! data assoc :interest-rate (.-target.value %)) false])
           (input-row "Number of years"
             [:input.form-control {:field :numeric :id :number-of-years :value (:number-of-years @data) :read-only true}])
-          (input-row "Percentage of down payment to be used in alternative investments"
+          (input-row "Percentage of down payment to be used in alternative investments (stocks, bonds, etc)"
             [:div
               [slider :percentage-invest percentage-invest 0 100]
               (str percentage-invest "%")])
-          (input-row "Money to invest" [money-input "Money to invest" :money-to-invest "" money-to-invest nil true])
+          (input-row "Total money to invest" [money-input "Money to invest" :money-to-invest "" money-to-invest nil true])
+          (input-row "Annual investment return rate (assumed)" [percentage-input "" :investment-return-rate "" investment-return-rate (update-val-fn :investment-return-rate) false])
+          (input-row "Total return on investment" [money-input "Total return on investment" :total-roi "" total-roi nil true])
           (input-row "Monthly rent" [money-input "Monthly rent" :monthly-rent "" monthly-rent (update-val-fn :monthly-rent) false])
-        ;   (input-row "Monthly payments" [money-input "Monthly payments" :monthly-payments "" monthly nil true])
-        ;   (input-row "Total interest cost" [money-input "Interst cost" :monthly-payments "" interest-cost nil true])
-        ;   (input-row "Ownership cost rate" [percentage-input "Ownership cost rate" :ownership-cost-rate "" (:ownership-cost-rate @data) #(swap! data assoc :ownership-cost-rate (.-target.value %)) false])
-        ;   (input-row "Annual ownership cost" [money-input "Annual ownership cost" :ownership-cost "" annual-ownership-cost nil true])
-        ;   (input-row "Monthly rent income" [money-input "Monthly rent income" :monthly-lease-income "" (:monthly-lease-income @data) #(swap! data assoc :monthly-lease-income (.-target.value %)) false])
-        ;   (input-row "Annual total cost (interest + maint - rent income)" [money-input "Annual total cost" :ownership-cost "" annual-cost nil true])
+          (input-row "Annual rent cost" [money-input "Annual rent cost" :annual-rent-cost "" annual-rent nil true])
+          (input-row (str "Total rent cost (over " years " years)") [money-input "Total rent cost" :total-rent-cost "" total-rent nil true])
           ]]))
 
 (defn to-own []
@@ -116,7 +120,7 @@
         annual-pnl (- (* monthly-lease-income 12) (+ annual-ownership-cost (/ interest-cost years)))
         total-pnl (* years annual-pnl)
         total-appreciation-gain ()]
-    [panel-with-heading "If buy (and invest)..."
+    [panel-with-heading "If buy (plus investments)..."
       [:form {:class "form-horizontal"}
         (input-row "Purchase price" [money-input "Purchase price" :purchase-price "" purchase-price (update-val-fn :purchase-price) false])
         (input-row "Total mortgage" [money-input "Total mortgage" :total-mortgage "" (:total-mortgage @data) (update-val-fn :total-mortgage) false])
@@ -126,14 +130,14 @@
         (input-row "Monthly mortgage you pay" [money-input "Monthly mortgage payment" :monthly-payments "" monthly nil true])
         (input-row (str "Total interest you pay (over " years " years)") [money-input "Interst cost" :monthly-payments "" interest-cost nil true])
         (input-row "Annual ownership cost rate (taxes + maint. fees + rennovation + etc, as a percentage of total purchase price)" [percentage-input "Ownership cost rate" :ownership-cost-rate "" (:ownership-cost-rate @data) (update-val-fn :ownership-cost-rate) false])
-        (input-row "Annual ownership cost" [money-input "Annual/monthly ownership cost" :ownership-cost "" annual-ownership-cost nil true])
-        (input-row "Monthly ownership cost" [money-input "Monthly ownership cost" :monthly-ownership-cost "" monthly-ownership-cost nil true])
-        (input-row "total monthly cost" [money-input "Monthly ownership cost" :monthly-total-cost "" monthly-total-cost nil true])
+        (input-row "Your annual ownership cost" [money-input "Annual/monthly ownership cost" :ownership-cost "" annual-ownership-cost nil true])
+        (input-row "Your monthly ownership cost" [money-input "Monthly ownership cost" :monthly-ownership-cost "" monthly-ownership-cost nil true])
+        (input-row "Your total monthly cost" [money-input "Monthly ownership cost" :monthly-total-cost "" monthly-total-cost nil true])
         (input-row "Monthly income you get from leasing" [money-input "Monthly rent income" :monthly-lease-income "" (:monthly-lease-income @data) (update-val-fn :monthly-lease-income) false])
-        (input-row "Total monthly profit/loss (income - cost)" [money-input "Monthly P&L" :monthly-pnl "" monthly-pnl nil true])
-        (input-row "Total annual profit/loss" [money-input "Annual total P&L" :ownership-cost "" annual-pnl nil true])
-        (input-row (str "Total profit/loss (over " years " years)") [money-input "Annual total P&L" :annual-pnl "" total-pnl nil true])
-        (input-row "Total gain from appreciation" [money-input "" :total-appreciation-gain "" "TODO" nil true])]]))
+        (input-row "Your total monthly profit/loss (income - cost)" [money-input "Monthly P&L" :monthly-pnl "" monthly-pnl nil true])
+        (input-row "Your total annual profit/loss" [money-input "Annual total P&L" :ownership-cost "" annual-pnl nil true])
+        (input-row (str "Your total profit/loss (over " years " years)") [money-input "Annual total P&L" :annual-pnl "" total-pnl nil true])
+        (input-row "Your total gain from appreciation" [money-input "" :total-appreciation-gain "" "TODO" nil true])]]))
 
 (defn rent-or-own-calculator []
   [:div.container
@@ -145,7 +149,7 @@
 
 (defn main-component []
   [:div
-    [:h2 "Rent vs. Buy Calculator"]
+    [:h2 "The Comprehensive Rent vs. Buy Calculator"]
     [rent-or-own-calculator]])
 
 (defn init []
